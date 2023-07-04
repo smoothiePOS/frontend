0692c7ab7c04a2f52c13d359aa3929c89cf1ef8d
Julius Vincent Babies <julius@familie-babies.de>
2023-07-04 08:04:15 +0200

    fixed init wrong function call

--- src/views/CashpointView.vue
+++ src/views/CashpointView.vue
@@ -162,7 +162,7 @@ export default defineComponent({
         }
     },
     mounted() {
-        this.update();
+        this.setup()
     },
     beforeUnmount() {
         clearInterval(this.timer);
