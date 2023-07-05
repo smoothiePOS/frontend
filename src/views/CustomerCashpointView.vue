15ec0ec82eddaaf5621c2c8ce01c326a8850d4a5
Julius Babies <julius@familie-babies.de>
2023-07-05 16:11:11 +0200

    - show ads in CustomerCashpointView.vue (#27)

--- src/views/CustomerCashpointView.vue
+++ src/views/CustomerCashpointView.vue
@@ -11,6 +11,11 @@ interface orderDetail {
 
 export default defineComponent({
     name: 'CustomerCashpointView',
+    computed: {
+        config() {
+            return config
+        }
+    },
     props: ['id'],
     data() {
         return {
@@ -23,6 +28,7 @@ export default defineComponent({
             items: 0,
             deposit: 0,
             sum: 0,
+            adUUID: ""
         }
     },
     mounted() {
@@ -30,10 +36,14 @@ export default defineComponent({
             this.dateString = new Date().toLocaleString()
             this.getOrder()
         }, 500)
+        this.getRandomAd()
+        setInterval(() => {
+            this.getRandomAd()
+        }, 10000)
     },
     methods: {
         fullscreen() {
-            //document.body.requestFullscreen()
+            document.body.requestFullscreen()
         },
         getOrder() {
             fetch(config.baseUrl + "/cashpoint/" + this.id + "/order/rt/customer")
@@ -56,6 +66,16 @@ export default defineComponent({
                     this.infoText = "Keine Serververbindung!"
                     this.problem = true
                 })
+        },
+        getRandomAd() {
+            let oldUUID = this.adUUID
+            fetch(config.baseUrl + "/product/random/available")
+                .then(response => response.text())
+                .then(data => {
+                    this.adUUID = data
+                    if (oldUUID == this.adUUID) this.getRandomAd()
+                })
+
         }
     }
 });
@@ -69,18 +89,25 @@ export default defineComponent({
                     <tr v-for="orderDetail in order" :key="orderDetail">
                         <td>{{ orderDetail.productName }}</td>
                         <td>x{{ orderDetail.amount }}</td>
-                        <td>{{ (orderDetail.price*orderDetail.amount/100).toFixed(2) }}€</td>
+                        <td>{{ (orderDetail.price * orderDetail.amount / 100).toFixed(2) }}€</td>
                     </tr>
                     <tr v-if="deposit != 0">
                         <td>Pfand</td>
                         <td>x{{ items }}</td>
-                        <td>{{ (deposit/100).toFixed(2) }}€</td>
+                        <td>{{ (deposit / 100).toFixed(2) }}€</td>
                     </tr>
                 </table>
             </div>
+            <div v-else id="cashpoint_customer_ad">
+                <img id="cashpoint_customer_ad_img" v-show="this.adUUID != ''"
+                     :src="config.advertisement.fullSizeImageBaseUrl + this.adUUID + config.advertisement.fullSizeImageExtension"
+                     alt="">
+            </div>
         </div>
         <div id="cashpoint_customer_footer"><span id="cashpoint_customer_footer_sum"
-                                                  v-if="this.order.length > 0">Summe: {{ ((sum+deposit)/100).toFixed(2) }}€</span><span v-else :class="this.problem ? 'blinking' : '' ">{{
+                                                  v-if="this.order.length > 0">Summe: {{
+                ((sum + deposit) / 100).toFixed(2)
+            }}€</span><span v-else :class="this.problem ? 'blinking' : '' ">{{
                 this.infoText
             }}</span><span id="cashpoint_customer_footer_date">{{ this.dateString }}</span></div>
     </div>
@@ -96,6 +123,21 @@ export default defineComponent({
     user-select: none;
 }
 
+#cashpoint_customer_ad {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    max-height: calc(100vh - 80px);
+}
+
+#cashpoint_customer_ad_img {
+    object-fit: contain;
+    max-width: 90vw;
+    max-height: calc(100vh - 150px);
+    border-radius: 10px;
+}
+
 #cashpoint_customer_data {
     position: fixed;
     display: flex;
@@ -103,7 +145,7 @@ export default defineComponent({
     padding-top: 20px;
     font-size: 40px;
     width: 100vw;
-    background: rgba(0, 0, 0, 0.7);
+    background: rgba(0, 0, 0, 0.3);
     height: calc(100vh - 80px);
 }
 
@@ -112,7 +154,7 @@ export default defineComponent({
     bottom: 0;
     height: 60px;
     width: 100vw;
-    background-color: rgba(0, 0, 0, 0.7);
+    background-color: rgba(0, 0, 0, 0.9);
     backdrop-filter: blur(10px);
     color: white;
     font-size: 40px;
@@ -127,6 +169,7 @@ export default defineComponent({
     padding-right: 20px;
 }
 
+/*noinspection CssUnusedSymbol*/
 .blinking {
     animation: blinkingText 1s infinite;
 }
