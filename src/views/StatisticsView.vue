6520fc8c0587c173844ef7f6e2407f4e792a13eb
Julius Vincent Babies <julius@familie-babies.de>
2023-07-05 18:23:14 +0200

    - stats

--- src/views/StatisticsView.vue
+++ src/views/StatisticsView.vue
@@ -4,13 +4,27 @@ import StatisticsComponent from "@/components/statistics/StatisticsComponent.vue
 
 export default defineComponent({
     name: 'StatisticsView',
-    components: {StatisticsComponent}
+    components: {StatisticsComponent},
+    data() {
+        return {
+            start: 0,
+            end: 0
+        }
+    },
+    beforeMount() {
+        const date = new Date()
+        date.setHours(0, 0, 0, 0)
+        this.start = date.getTime()/1000
+
+        date.setHours(23, 59, 59, 999)
+        this.end = Math.floor(date.getTime()/1000)
+    }
 })
 </script>
 
 <template>
 <h1>Statistics</h1>
-    <StatisticsComponent to="0" from="0"></StatisticsComponent>
+    <StatisticsComponent :start="this.start" :end="this.end"></StatisticsComponent>
 </template>
 
 <style scoped>
